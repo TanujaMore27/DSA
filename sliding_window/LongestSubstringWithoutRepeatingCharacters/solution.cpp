a39df57c9480575d72ab9d97bcd6e79f5951975c
TanujaMore27 <moretanuja971@gmail.com>
2026-02-18 21:02:22 +0530

    added LongestSubstringWithoutRepeatingCharacters

---
+++ sliding_window/LongestSubstringWithoutRepeatingCharacters/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int> freq;
+        int left = 0;
+        int maxlen = 0;
+        for(int right = 0;right<s.length();right++){
+            int ch = s[right];
+            if(freq.find(ch) != freq.end()){
+                left = max(left,freq[ch]+1);
+            }
+            freq[ch] = right;
+            maxlen = max(maxlen,right-left+1);
+        }
+        return maxlen;
+    }
+};
